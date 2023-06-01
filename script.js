bde373496545234f855d1f585b1f0c66a82787dd
Muhammad Nizar Hazmi <76721471+neexur@users.noreply.github.com>
2023-06-01 07:18:08 +0000

    yuyu

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ const timing = {duration: 500, easing: "cubic-bezier(0,.48,.28,1.26)"};
 
 function updateContents(city) {
   let APIKey = "0306ed3057f03485ffb9efd56c98131c";
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},%20MY&units=metric&appid=${APIKey}`)
   .then(response => response.json()).then(json => {
     ["temp", "temp_max", "temp_min"].forEach(data => {
       divContent.querySelector(`span#${data}`).innerHTML = Math.round(json.main[data]) + "°C";
     });
+    divContent.querySelector("span.description#description").innerHTML = json.weather[0]["description"].split("").map((l, o) => o === 0 ? l.toUpperCase() : l).join("");
     ["humidity", "pressure"].forEach(data => {
       divContent.querySelector(`span.value#${data}`).innerHTML = json.main[data];
     })
-    divContent.querySelector("span.value#wind").innerHTML = json.wind.speed;
-    divContent.querySelector("span.description#description").innerHTML = json.weather[0]["description"].split("").map((l, o) => o === 0 ? l.toUpperCase() : l).join("");
+    divContent.querySelector("span.value#wind").textContent = json.wind.speed;
   })
 }
 
